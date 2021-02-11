c96c3d8887afae31e643870f637c53fda8ffe487
Alexander Urban <lexa@toxa.de>
2021-02-12 00:43:44 +0100

    Dummy-DB storage revised

--- src/dummy-db/entities/users.storage.js
+++ src/dummy-db/entities/users.storage.js
@@ -1,5 +1,12 @@
 const getTestId = require('../getTestId');
 
+const _getLearnPathItem = (step, wordIdKey, flectionIdKey, result) => ({
+  step,
+  wordId: getTestId(wordIdKey),
+  flectionId: getTestId(flectionIdKey),
+  result,
+});
+
 const allUsers = [
   {
     id: getTestId('user1'),
@@ -7,17 +14,8 @@ const allUsers = [
     name: 'User 1',
     currStep: 8,
     learnPath: [
-      {
-        step: 7,
-        wordId: getTestId('word3'),
-        flection: getTestId('adjective-flection2'),
-        result: 100,
-      },
-      {
-        step: 8,
-        wordId: getTestId('word1'),
-        flection: getTestId('verb-flection4'),
-      },
+      _getLearnPathItem(7, 'word3', 'adjective-flection2', 100),
+      _getLearnPathItem(8, 'word1', 'verb-flection4', null),
     ],
   },
   {
@@ -28,52 +26,29 @@ const allUsers = [
   },
 ];
 
+const _getProgressItem = (flectionIdKey, success, failure, lastStep, lastResult, lastTimestamp) => {
+  const flectionId = getTestId(flectionIdKey);
+  return {
+    flectionId,
+    success,
+    failure,
+    lastStep,
+    lastResult,
+    lastTimestamp,
+  };
+};
+
 const allActiveWords = [
   {
     id: getTestId('user1-activeWord1'),
     userId: getTestId('user1'),
     wordId: getTestId('word1'),
     learnProgress: [
-      {
-        flectionId: getTestId('verb-flection1'),
-        success: 0,
-        failure: 0,
-        lastStep: null,
-        lastResult: null,
-        lastTimestamp: null,
-      },
-      {
-        flectionId: getTestId('verb-flection2'),
-        success: 0,
-        failure: 0,
-        lastStep: null,
-        lastResult: null,
-        lastTimestamp: null,
-      },
-      {
-        flectionId: getTestId('verb-flection3'),
-        success: 0,
-        failure: 0,
-        lastStep: null,
-        lastResult: null,
-        lastTimestamp: null,
-      },
-      {
-        flectionId: getTestId('verb-flection4'),
-        success: 0,
-        failure: 0,
-        lastStep: null,
-        lastResult: null,
-        lastTimestamp: null,
-      },
-      {
-        flectionId: getTestId('verb-flection5'),
-        success: 0,
-        failure: 0,
-        lastStep: null,
-        lastResult: null,
-        lastTimestamp: null,
-      },
+      _getProgressItem('verb-flection1', 0, 0, null, null, null),
+      _getProgressItem('verb-flection2', 0, 0, null, null, null),
+      _getProgressItem('verb-flection3', 0, 0, null, null, null),
+      _getProgressItem('verb-flection4', 0, 0, null, null, null),
+      _getProgressItem('verb-flection5', 0, 0, null, null, null),
     ],
   },
   {
@@ -81,54 +56,12 @@ const allActiveWords = [
     userId: getTestId('user1'),
     wordId: getTestId('word3'),
     learnProgress: [
-      {
-        flectionId: getTestId('adjective-flection1'),
-        success: 0,
-        failure: 0,
-        lastStep: null,
-        lastResult: null,
-        lastTimestamp: null,
-      },
-      {
-        flectionId: getTestId('adjective-flection2'),
-        success: 0,
-        failure: 0,
-        lastStep: null,
-        lastResult: null,
-        lastTimestamp: null,
-      },
-      {
-        flectionId: getTestId('adjective-flection3'),
-        success: 0,
-        failure: 0,
-        lastStep: null,
-        lastResult: null,
-        lastTimestamp: null,
-      },
-      {
-        flectionId: getTestId('adjective-flection4'),
-        success: 0,
-        failure: 0,
-        lastStep: null,
-        lastResult: null,
-        lastTimestamp: null,
-      },
-      {
-        flectionId: getTestId('adjective-flection5'),
-        success: 0,
-        failure: 0,
-        lastStep: null,
-        lastResult: null,
-        lastTimestamp: null,
-      },
-      {
-        flectionId: getTestId('adjective-flection6'),
-        success: 0,
-        failure: 0,
-        lastStep: null,
-        lastResult: null,
-        lastTimestamp: null,
-      },
+      _getProgressItem('adjective-flection1', 0, 0, null, null, null),
+      _getProgressItem('adjective-flection2', 0, 0, null, null, null),
+      _getProgressItem('adjective-flection3', 0, 0, null, null, null),
+      _getProgressItem('adjective-flection4', 0, 0, null, null, null),
+      _getProgressItem('adjective-flection5', 0, 0, null, null, null),
+      _getProgressItem('adjective-flection6', 0, 0, null, null, null),
     ],
   },
 ];
--- src/dummy-db/entities/words.storage.js
+++ src/dummy-db/entities/words.storage.js
@@ -15,140 +15,47 @@ const allWords = [
   },
 ];
 
+const _getTranslationItems = (wordIdKey, ...listOfFlectionIdKeyWithTextTuple) => {
+  const wordId = getTestId(wordIdKey);
+  // eslint-disable-next-line camelcase
+  return listOfFlectionIdKeyWithTextTuple.map(([flectionIdKey, text_de, text_sv], index) => ({
+    id: getTestId(`${wordIdKey}-translation${index + 1}`),
+    wordId,
+    flectionId: getTestId(flectionIdKey),
+    text_de,
+    text_sv,
+  }));
+};
+
 const allTranslations = [
-  {
-    id: getTestId('word1-translation1'),
-    wordId: getTestId('word1'),
-    flectionId: getTestId('verb-flection1'),
-    text_de: 'gehen',
-    text_sv: 'gå',
-  },
-  {
-    id: getTestId('word1-translation2'),
-    wordId: getTestId('word1'),
-    flectionId: getTestId('verb-flection2'),
-    text_de: 'er geht',
-    text_sv: 'han går',
-  },
-  {
-    id: getTestId('word1-translation3'),
-    wordId: getTestId('word1'),
-    flectionId: getTestId('verb-flection2'),
-    text_de: 'sie geht',
-    text_sv: 'hon går',
-  },
-  {
-    id: getTestId('word1-translation4'),
-    wordId: getTestId('word1'),
-    flectionId: getTestId('verb-flection2'),
-    text_de: 'wir gehen',
-    text_sv: 'vi går',
-  },
-  {
-    id: getTestId('word1-translation5'),
-    wordId: getTestId('word1'),
-    flectionId: getTestId('verb-flection3'),
-    text_de: 'er ging',
-    text_sv: 'han gick',
-  },
-  {
-    id: getTestId('word1-translation6'),
-    wordId: getTestId('word1'),
-    flectionId: getTestId('verb-flection4'),
-    text_de: 'er ist gegangen',
-    text_sv: 'han har gått',
-  },
-  {
-    id: getTestId('word1-translation7'),
-    wordId: getTestId('word1'),
-    flectionId: getTestId('verb-flection5'),
-    text_de: 'geh!',
-    text_sv: 'gå!',
-  },
-  {
-    id: getTestId('word2-translation1'),
-    wordId: getTestId('word2'),
-    flectionId: getTestId('noun-flection1'),
-    text_de: 'ein Haus',
-    text_sv: 'ett hus',
-  },
-  {
-    id: getTestId('word2-translation2'),
-    wordId: getTestId('word2'),
-    flectionId: getTestId('noun-flection2'),
-    text_de: 'das Haus',
-    text_sv: 'huset',
-  },
-  {
-    id: getTestId('word2-translation3'),
-    wordId: getTestId('word2'),
-    flectionId: getTestId('noun-flection3'),
-    text_de: 'viele Häuser',
-    text_sv: 'många hus',
-  },
-  {
-    id: getTestId('word2-translation4'),
-    wordId: getTestId('word2'),
-    flectionId: getTestId('noun-flection4'),
-    text_de: 'die Häuser',
-    text_sv: 'husen',
-  },
-  {
-    id: getTestId('word3-translation1'),
-    wordId: getTestId('word3'),
-    flectionId: getTestId('adjective-flection1'),
-    text_de: 'ein altes Auto',
-    text_sv: 'en gammal bil',
-  },
-  {
-    id: getTestId('word3-translation2'),
-    wordId: getTestId('word3'),
-    flectionId: getTestId('adjective-flection2'),
-    text_de: 'ein altes Haus',
-    text_sv: 'ett gammalt hus',
-  },
-  {
-    id: getTestId('word3-translation3'),
-    wordId: getTestId('word3'),
-    flectionId: getTestId('adjective-flection3'),
-    text_de: 'viele alte Autos',
-    text_sv: 'många gamla bilar',
-  },
-  {
-    id: getTestId('word3-translation4'),
-    wordId: getTestId('word3'),
-    flectionId: getTestId('adjective-flection4'),
-    text_de: 'das alte Auto',
-    text_sv: 'den gamla bilen',
-  },
-  {
-    id: getTestId('word3-translation5'),
-    wordId: getTestId('word3'),
-    flectionId: getTestId('adjective-flection4'),
-    text_de: 'das alte Haus',
-    text_sv: 'det gamla huset',
-  },
-  {
-    id: getTestId('word3-translation6'),
-    wordId: getTestId('word3'),
-    flectionId: getTestId('adjective-flection4'),
-    text_de: 'die alten Autos',
-    text_sv: 'de gamla bilarna',
-  },
-  {
-    id: getTestId('word3-translation7'),
-    wordId: getTestId('word3'),
-    flectionId: getTestId('adjective-flection5'),
-    text_de: 'älter',
-    text_sv: 'äldre',
-  },
-  {
-    id: getTestId('word3-translation8'),
-    wordId: getTestId('word3'),
-    flectionId: getTestId('adjective-flection6'),
-    text_de: 'am ältesten',
-    text_sv: 'äldst',
-  },
+  ..._getTranslationItems(
+    'word1',
+    ['verb-flection1', 'gehen', 'gå'],
+    ['verb-flection2', 'er geht', 'han går'],
+    ['verb-flection2', 'sie geht', 'hun går'],
+    ['verb-flection2', 'wir gehen', 'vi går'],
+    ['verb-flection3', 'er ging', 'han gick'],
+    ['verb-flection4', 'er ist gegangen', 'han har gått'],
+    ['verb-flection5', 'geh!|geh', 'gå!|gå']
+  ),
+  ..._getTranslationItems(
+    'word2',
+    ['noun-flection1', 'ein Haus', 'ett hus'],
+    ['noun-flection2', 'das Haus', 'huset'],
+    ['noun-flection3', 'viele Häuser', 'många hus'],
+    ['noun-flection4', 'die Häuser', 'husen']
+  ),
+  ..._getTranslationItems(
+    'word3',
+    ['adjective-flection1', 'ein altes Auto', 'en gammal bil'],
+    ['adjective-flection2', 'ein altes Haus', 'ett gammalt hus'],
+    ['adjective-flection3', 'viele alte Autos', 'många gamla bilar'],
+    ['adjective-flection4', 'das alte Auto', 'den gamla bilen'],
+    ['adjective-flection4', 'das alte Haus', 'det gamla huset'],
+    ['adjective-flection4', 'die alten Autos', 'de gamla bilarna'],
+    ['adjective-flection5', 'älter', 'äldre'],
+    ['adjective-flection6', 'am ältesten', 'äldst']
+  ),
 ];
 
 module.exports = {
