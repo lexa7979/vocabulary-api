113b692a626e7c46b604c0a754dbab48b1c15bf0
Alexander Urban <lexa@toxa.de>
2021-02-12 00:53:04 +0100

    Dummy-DB storage revised

--- src/dummy-db/entities/wordClasses.storage.js
+++ src/dummy-db/entities/wordClasses.storage.js
@@ -15,97 +15,35 @@ const allWordClasses = [
   },
 ];
 
+const _getFlectionItems = (classIdKey, ...listOfFlectionNames) => {
+  const classId = getTestId(classIdKey);
+  // eslint-disable-next-line camelcase
+  return listOfFlectionNames.map((name_de, index) => ({
+    id: getTestId(`${classIdKey}-flection${index + 1}`),
+    classId,
+    pos: index,
+    name_de,
+  }));
+};
+
 const allFlections = [
-  {
-    id: getTestId('verb-flection1'),
-    classId: getTestId('verb'),
-    pos: 0,
-    name_de: 'Infinitiv',
-  },
-  {
-    id: getTestId('verb-flection2'),
-    classId: getTestId('verb'),
-    pos: 1,
-    name_de: 'Präsens',
-  },
-  {
-    id: getTestId('verb-flection3'),
-    classId: getTestId('verb'),
-    pos: 2,
-    name_de: 'Präteritum',
-  },
-  {
-    id: getTestId('verb-flection4'),
-    classId: getTestId('verb'),
-    pos: 3,
-    name_de: 'Perfekt',
-  },
-  {
-    id: getTestId('verb-flection5'),
-    classId: getTestId('verb'),
-    pos: 4,
-    name_de: 'Imperativ',
-  },
-  {
-    id: getTestId('noun-flection1'),
-    classId: getTestId('noun'),
-    pos: 0,
-    name_de: 'Singular, unbestimmt',
-  },
-  {
-    id: getTestId('noun-flection2'),
-    classId: getTestId('noun'),
-    pos: 1,
-    name_de: 'Singular, bestimmt',
-  },
-  {
-    id: getTestId('noun-flection3'),
-    classId: getTestId('noun'),
-    pos: 2,
-    name_de: 'Plural, unbestimmt',
-  },
-  {
-    id: getTestId('noun-flection4'),
-    classId: getTestId('noun'),
-    pos: 3,
-    name_de: 'Plural, bestimmt',
-  },
-  {
-    id: getTestId('adjective-flection1'),
-    classId: getTestId('adjective'),
-    pos: 0,
-    name_de: 'Grundstufe, utrum unbestimmt',
-  },
-  {
-    id: getTestId('adjective-flection2'),
-    classId: getTestId('adjective'),
-    pos: 1,
-    name_de: 'Grundstufe, neutrum unbestimmt',
-  },
-  {
-    id: getTestId('adjective-flection3'),
-    classId: getTestId('adjective'),
-    pos: 2,
-    name_de: 'Grundstufe, Plural unbestimmt',
-  },
-  {
-    id: getTestId('adjective-flection4'),
-    classId: getTestId('adjective'),
-    pos: 3,
-    name_de: 'Grundstufe, bestimmt',
-  },
-  {
-    id: getTestId('adjective-flection5'),
-    classId: getTestId('adjective'),
-    pos: 4,
-    name_de: 'Komparativ',
-  },
-  {
-    id: getTestId('adjective-flection6'),
-    classId: getTestId('adjective'),
-    pos: 5,
-    name_de: 'Superlativ',
-  },
+  ..._getFlectionItems('verb', 'Infinitiv', 'Präsens', 'Präteritum', 'Perfekt', 'Imperativ'),
+  ..._getFlectionItems(
+    'noun',
+    'Singular, unbestimmt',
+    'Singular, bestimmt',
+    'Plural, unbestimmt',
+    'Plural, bestimmt'
+  ),
+  ..._getFlectionItems(
+    'adjective',
+    'Grundstufe, utrum unbestimmt',
+    'Grundstufe, neutrum unbestimmt',
+    'Grundstufe, Plural unbestimmt',
+    'Grundstufe, bestimmt',
+    'Komparativ',
+    'Superlativ'
+  ),
 ];
 
 module.exports = {
