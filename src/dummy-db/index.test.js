87332bccdd4cb2d8da164b0c436e353a49956a10
Alexander Urban <lexa@toxa.de>
2021-03-11 01:08:01 +0100

    Unit tests revised

--- src/dummy-db/index.test.js
+++ src/dummy-db/index.test.js
@@ -2,49 +2,47 @@
 // const DummyDB = jest.requireActual('./index');
 // const DummyDBMockup = jest.requireMock('./index');
 
+const { copyObject } = require('../../test');
+
 const DummyDB = require('./index');
 
 describe('Dummy-DB API', () => {
   it('has all expected exports', () => {
-    expect(DummyDB).toMatchInlineSnapshot(`
-      Object {
-        "getActiveWord": [Function],
-        "getFlection": [Function],
-        "getTranslation": [Function],
-        "getUser": [Function],
-        "getWord": [Function],
-        "getWordClass": [Function],
-        "getWordClassWithFlections": [Function],
-        "getWordWithTranslations": [Function],
-        "listAllActiveWordsOfUser": [Function],
-        "listAllFlectionsOfWordClass": [Function],
-        "listAllTranslationsOfWord": [Function],
-        "listAllUsers": [Function],
-        "listAllWordClasses": [Function],
-        "listAllWords": [Function],
-      }
-    `);
+    expect(copyObject(DummyDB, { replaceFunctions: true })).toEqual({
+      getActiveWord: '(FUNC:getActiveWord)',
+      getFlection: '(FUNC:getFlection)',
+      getTranslation: '(FUNC:getTranslation)',
+      getUser: '(FUNC:getUser)',
+      getWord: '(FUNC:getWord)',
+      getWordClass: '(FUNC:getWordClass)',
+      getWordClassWithFlections: '(FUNC:getWordClassWithFlections)',
+      getWordWithTranslations: '(FUNC:getWordWithTranslations)',
+      listAllActiveWordsOfUser: '(FUNC:listAllActiveWordsOfUser)',
+      listAllFlectionsOfWordClass: '(FUNC:listAllFlectionsOfWordClass)',
+      listAllTranslationsOfWord: '(FUNC:listAllTranslationsOfWord)',
+      listAllUsers: '(FUNC:listAllUsers)',
+      listAllWordClasses: '(FUNC:listAllWordClasses)',
+      listAllWords: '(FUNC:listAllWords)',
+    });
   });
 
   it(`has exports with the expected number of arguments`, () => {
-    expect(_getNumberOfArgumentsOfEveryFunction(DummyDB)).toMatchInlineSnapshot(`
-      Object {
-        "getActiveWord": 1,
-        "getFlection": 1,
-        "getTranslation": 1,
-        "getUser": 1,
-        "getWord": 1,
-        "getWordClass": 1,
-        "getWordClassWithFlections": 1,
-        "getWordWithTranslations": 1,
-        "listAllActiveWordsOfUser": 1,
-        "listAllFlectionsOfWordClass": 1,
-        "listAllTranslationsOfWord": 1,
-        "listAllUsers": 0,
-        "listAllWordClasses": 0,
-        "listAllWords": 0,
-      }
-    `);
+    expect(_getNumberOfArgumentsOfEveryFunction(DummyDB)).toEqual({
+      getActiveWord: 1,
+      getFlection: 1,
+      getTranslation: 1,
+      getUser: 1,
+      getWord: 1,
+      getWordClass: 1,
+      getWordClassWithFlections: 1,
+      getWordWithTranslations: 1,
+      listAllActiveWordsOfUser: 1,
+      listAllFlectionsOfWordClass: 1,
+      listAllTranslationsOfWord: 1,
+      listAllUsers: 0,
+      listAllWordClasses: 0,
+      listAllWords: 0,
+    });
   });
 
   // it.skip(`has a mockup with equal exports`, () => {
